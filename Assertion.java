b0f07cbbb43cf412f1bc6328c76c27a8b283b8bf
Prashant Pandey <23prashantpandey@gmail.com>
2026-07-31 10:03:03 +0530

    Add files via upload

---
+++ Assertion.java
@@ -0,0 +1,10 @@
+class Ass {
+    public static void main(String ar[]) {
+        int x = 7;
+        assert (true);
+        if (x == 8) {
+            System.out.println("Number");
+        }
+        System.out.println("out");
+    }
+}
