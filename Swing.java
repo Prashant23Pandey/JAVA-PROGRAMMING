b4279651ae6dca71eef81433b00f63117c6d22a5
Prashant Pandey <23prashantpandey@gmail.com>
2026-08-08 19:25:33 +0530

    Add files via upload

---
+++ Swing.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import javax.swing.*;
+class SWING{
+public static void main(String[] args)
+    }
+JFrame frame = new JFrame();
+JButton button = new JButton("Click");
+button.setBound(150,200,220,50);
+frame.add(button);
+frame.setSize(500,600);
+frame.setLayout(null);
+frame.setvisible(true);
